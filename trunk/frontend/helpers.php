376d95dfbe2d12adb9b4999d74a7020de54b6cf9
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2010-04-07 21:00:54 +0000

    Frontend changes

--- trunk/frontend/helpers.php
+++ trunk/frontend/helpers.php
@@ -8,6 +8,15 @@ function p($str)
 
 class GuiHelpers
 {
+    private static $allowed_pages = array ('dashboard', 'contacts', 'config');
+
+    public static function getPage($requested)
+    {
+        if(in_array($requested, GuiHelpers::$allowed_pages))
+            return $requested;
+        return 'dashboard';
+    }
+
     public static function getMonitors()
     {
         $monitors = array();
@@ -15,6 +24,30 @@ class GuiHelpers
         foreach($mrows as $m)
             $monitors[] = Monitor::fetch($m);
         return $monitors;
+    }
+
+    public static function getContactsByMonitor($monitor)
+    {
+        return $GLOBALS['PW_DB']->executeRaw('SELECT DISTINCT contacts.id, contacts.name FROM contacts, channels WHERE channels.owner =
+        contacts.id AND channels.id IN (' . implode(',', $monitor->getChanIds()) . ')');
+    }
+
+    public static function getStatistic($field)
+    {
+        $r = $GLOBALS['PW_DB']->executeRaw('SELECT COUNT(monitors.id) AS monitor_count, COUNT(contacts.id) AS contact_count
+        FROM monitors, contacts');
+        $r = $r[0];
+        $lcount = $GLOBALS['PW_DB']->executeRaw('SELECT COUNT(*) AS log_count FROM statistics');
+        $r['log_count'] = $lcount[0]['log_count'];
+        $offline = $GLOBALS['PW_DB']->executeRaw('SELECT MAX(`key`) AS last_offline FROM `statistics` WHERE series LIKE \'monitor%\' AND
+        value = 0');
+        $r['last_offline'] = $offline[0]['last_offline'];
+        return $r[$field];
+    }
+
+    public static function formatDateLong($timestamp)
+    {
+        return date('D, M j, Y G:i:s T', $timestamp);
     }
 }
 ?>
--- trunk/frontend/index.php
+++ trunk/frontend/index.php
@@ -15,37 +15,28 @@
 			<div id="left-column">				
 				<div id="subheader">
 					<img src="images/logo.jpg">
-					<p>System Dashboard</p>
-					<p class="right-side">Current version: v2.0</p>
+					<p><?php p(GuiHelpers::getPage($_GET['page'])); ?></p>
+					<p class="right-side">Current version: v<?php p(PW2_VERSION); ?></p>
 				</div>
-                <div id="menu">
+                <div class="menu">
                 <ul>
                     <li><a href="#">Dashboard</a></li>
                     <li><a href="#">Contacts</a></li>
                     <li><a href="#">Configuration</a></li>
                 </ul>
                 </div>
-				<div class="section">
-					<h1>Monitors</h1>
-                    <?php foreach(GuiHelpers::getMonitors() as $monitor) : ?>
-                    <h2><?php p($monitor->getAlias()); ?></h2> (IP: <?php p($monitor->getHostname()); ?>, port <?php p($monitor->getPort()); ?>)
-					<ul class="information">
-						<li><strong>Contacts:</strong> Aaron Rosenfeld, Person Two, Person Three, <a href="#">and 5 others...</a></li>
-                        <li><strong>Last Offline:</strong> February 15, 2010 13:22 GMT</li>
-					</ul>
-                    <?php endforeach; ?>
-				</div>
+                <?php require('./pages/' . GuiHelpers::getPage($_GET['page']) . '.php'); ?>
 			</div>
 			
 			<div class="right-block">
 				<p class="side-title">Total Monitors:</p>
-				<p>12</p>
+				<p><?php p(GuiHelpers::getStatistic('monitor_count')); ?></p>
+				<p class="side-title">Total Contacts:</p>
+				<p><?php p(GuiHelpers::getStatistic('contact_count')); ?></p>
+				<p class="side-title">Total Log Entries:</p>
+				<p><?php p(GuiHelpers::getStatistic('log_count')); ?></p>
 				<p class="side-title">Last Offline:</p>
-				<p>February 15, 2010 13:22 GMT</p>
-				<p class="side-title">Total Log Messages:</p>
-				<p>123,418,929</p>
-				<p class="side-title">Avg. Uptime:</p>
-                <p>78.42%</p>
+                <p><?php p(GuiHelpers::formatDateLong(GuiHelpers::getStatistic('last_offline'))); ?></li>
 			</div>
 		</div>
 	</body>
