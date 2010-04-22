b2f39e1ff20b464415f17c03a8cc89a545c9b57a
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2010-04-22 17:37:26 +0000

    Bump version

--- trunk/config.php
+++ trunk/config.php
@@ -10,6 +10,6 @@
         'path' => dirname(__FILE__),
     );
 
-    define('PW2_VERSION', '2.0.3 Beta');
+    define('PW2_VERSION', '2.0.4 Beta');
     define('PW2_PATH', $PW2_CONFIG['path']);
 ?>
